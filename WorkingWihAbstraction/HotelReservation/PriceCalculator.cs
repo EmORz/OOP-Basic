4d3b8b25a8c8c70f9d20904f334cd4bb252510cc
Emo <eiv.razgrad@gmail.com>
2018-10-20 18:30:15 +0300

    Add

---
+++ WorkingWihAbstraction/HotelReservation/PriceCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservation
+{
+    public static class PriceCalculator
+    {
+        public static decimal Calculate(decimal price, int days, Season seasons, Discount typeClient)
+        {
+            var temp = (price * days) * (int)seasons * ((100-(decimal)typeClient)/100);
+            return temp;
+        }
+
+        public static decimal CalculateS(decimal price, int days, Season seasons)
+        {
+            var temp = (price * days) * (int)seasons;
+            return temp;
+        }
+    }
+}
---
+++ WorkingWihAbstraction/HotelReservation/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HotelReservation
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            //
+            if (input.Length==4)
+            {
+                decimal price = decimal.Parse(input[0]);
+                int days = int.Parse(input[1]);
+                var sea = input[2];
+                var dis = input[3];
+                Enum.TryParse(sea, out Season season);
+                Enum.TryParse(dis, out Discount discount);
+                var resulT = PriceCalculator.Calculate(price, days, season, discount);
+                Console.WriteLine($"{resulT:f2}");
+            }
+            else
+            {
+                decimal price = decimal.Parse(input[0]);
+                int days = int.Parse(input[1]);
+                Enum.TryParse(input[2], out Season season);
+                var resulT = PriceCalculator.CalculateS(price, days, season);
+                Console.WriteLine($"{resulT:f2}");
+            }
+
+
+
+        }
+    }
+}
