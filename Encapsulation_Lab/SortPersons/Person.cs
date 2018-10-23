c84d40889c479789bd819a7e86284ac8330111a5
Emo <eiv.razgrad@gmail.com>
2018-10-23 11:57:31 +0300

    Add

---
+++ Encapsulation_Lab/SortPersons/Person.cs
@@ -0,0 +1,28 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName= firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+        //
+        public string FisrtsName => this.firstName;
+        public string LastName => this.lastName;
+        public int Age => this.age;
+ 
+        public override string ToString()
+        {
+            return $"{this.FisrtsName} {this.LastName} is {this.Age} years old.";
+        }
+
+
+
+    }
+}
---
+++ Encapsulation_Lab/SortPersons/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace PersonsInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string firstName = input[0];
+                string lastName = input[1];
+                int age = int.Parse(input[2]);
+                //
+                Person current = new Person(firstName, lastName, age);
+                persons.Add(current);
+            }
+            persons.OrderBy(p => p.FisrtsName).ThenBy(a => a.Age).ToList().ForEach(per => Console.WriteLine(per.ToString())) ;
+        }
+    }
+}
