4e4016b12528568241d1ae34d02e6b42ace4adf8
Emo <eiv.razgrad@gmail.com>
2018-10-27 20:51:14 +0300

    Add

---
+++ Inheritance_Exercise/Animals/Animals/Animal.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm.Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        public Animal(string name, int age, string gender)
+        {
+           this.Name = name;
+           this.Age = age;
+           this.Gender = gender;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (String.IsNullOrEmpty(value)||value == null)
+                {
+                    throw new Exception("Invalid input!");
+                }
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Invalid input!");
+                }
+                age = value;
+            }
+        }
+        public string Gender
+        {
+            get { return gender; }
+            private set
+            {
+                if (String.IsNullOrEmpty(value) || value == null)
+                {
+                    throw new Exception("Invalid input!");
+                }
+                gender = value;
+            }
+        }
+
+        public virtual void ProduceSound()
+        {
+            Console.WriteLine("Screeeeeeeeeeeem :(");
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Age} {this.Gender}";
+        }
+
+
+
+
+    }
+}
---
+++ Inheritance_Exercise/Animals/StartUp.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Farm
+{
+    using Farm.Core;
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
