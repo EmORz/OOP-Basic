d2e2ab43d49411801a059ce6b37a898c5f2f6b10
Emo <eiv.razgrad@gmail.com>
2018-11-01 17:11:44 +0200

    Add

---
+++ ExcercisesInterfaceAndAbstraction/MilitaryElite/Contracts/ILieutenantGeneral.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace MilitaryElite.Contracts
+{
+    public interface ILieutenantGeneral : IPrivate
+    {
+        IReadOnlyCollection<IPrivate> Privates { get; }
+    }
+}
---
+++ ExcercisesInterfaceAndAbstraction/MilitaryElite/Models/LieutenantGeneral.cs
@@ -0,0 +1,39 @@
+﻿using MilitaryElite.Contracts;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models
+{
+    public class LieutenantGeneral : Private, ILieutenantGeneral
+    {
+        private List<Private> privates;
+        public LieutenantGeneral(string id, string firstName, string lastName, decimal salary) :
+            base(id, firstName, lastName, salary)
+        {
+            this.privates = new List<Private>();
+        }
+        public IReadOnlyCollection<IPrivate> Privates
+        {
+            get
+            {
+                return this.privates;
+            }
+        }
+        public void AddPrivate(Private @privates)
+        {
+            this.privates.Add(@privates);
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(base.ToString())
+                .AppendLine("Privates:");
+
+            foreach (var item in privates)
+            {
+                sb.AppendLine("  "+item.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
